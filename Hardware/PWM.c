8c5306be69ad40769bd7f56e32fc1e81a430ed5c
Kansas_Lauag <78003801+L22000011@users.noreply.github.com>
2023-06-29 12:21:53 +0800

    Update PWM.c

--- Hardware/PWM.c
+++ Hardware/PWM.c
@@ -33,7 +33,7 @@ void PWM_Init_DJ(void)
 	TIM_Cmd(TIM3, ENABLE);
 }
 void PWM_Init(void)
-{
+{   //TIM2舵机驱动
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	
